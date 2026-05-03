fae574b5dd1b0420a20f16439a3185faeff96d28
Shalini Thondapu <shalinithondapu@Shalinis-MacBook-Air.local>
2026-05-03 16:37:02 +0530

    made changes

--- practice1.js
+++ practice1.js
@@ -1,9 +1,48 @@
-var n=2;
-function square(num){
-    for(i=1;i<=num;i++){
-        document.write(i*i + "<br>");
-    }
+// var n=0;
+// function square(num){
+//     for(i=1;i<=num;i++){
+//         document.write(i*i + "<br>");
+//     }
     
+// }
+// var square2 = square(n);
+
+
+
+
+// for(i=1;i<=10;i++){
+//     for(j=1;j<=10;j++){
+//         document.write(i*j +" ");
+//     }
+//     document.write("<br>");
+// }
+
+// let num1 = 5;
+
+// let result = num1*5;
+
+// for(i=num1;)
+
+
+//  for (i=5;i<=5;i=i+20){
+//  for(j=5;j<=i;j=j*5){
+//     document.write(j + "<br>")
+//         }
+//      }
+
+let result = 5;
+for (let i = 1; i <= 4; i++) {
+    console.log(result);
+    result = result*5;
 }
-var square2 = square(n);
-document.write(square2());
+
+
+//
+
+
+
+//25
+//125
+//625
+//3125
+
